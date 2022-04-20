6c4ef68d78d736d7e0435f1cd7f69af115304ab2
Felix Wu <felix@2simple.com>
2022-04-21 00:15:05 +0100

    add my own merges

--- run.py
+++ run.py
@@ -1,25 +1,168 @@
 from lib.lib import start
 
 # destination playlists
+release_discovery     = 'https://open.spotify.com/playlist/1MEp4PrZZUdwUnEGvT7qYb?si=0a3c57580c684982'
+all_daily_mixes       = 'https://open.spotify.com/playlist/1QacCtP1WONJMnLEKY1Bbq?si=fb1df6310ada410f&pt=31d4a44991e03cd19de6bcca69a7de8e'
+urban_mood            = 'https://open.spotify.com/playlist/6QyPXogZHDElyNcXRf14oN'
+upbeat_mood           = 'https://open.spotify.com/playlist/6ip96AlGVRpb41U8cOudo6'
+funky_mood            = 'https://open.spotify.com/playlist/3Xw80hGkazf4qOPIw30SpJ'
+clubby_mood           = 'https://open.spotify.com/playlist/3o2U5B7cvlFspKuFEDJyhq'
+breaks_mood           = 'https://open.spotify.com/playlist/62RrxLSRtbJAhiQAPu0jEh'
+deep_mood             = 'https://open.spotify.com/playlist/6mBSlC6XZHSL0UyWFAGUGO'
+chill_mood            = 'https://open.spotify.com/playlist/6dtETOTmLhXAZZZa2DH9Cb'
+electronic_mood       = 'https://open.spotify.com/playlist/0Y2InqMR3vNJeRvvttZfR9'
+release_discovery     = 'https://open.spotify.com/playlist/1MEp4PrZZUdwUnEGvT7qYb'
 all_daily_mixes       = 'https://open.spotify.com/playlist/1QacCtP1WONJMnLEKY1Bbq'
-# add more playlists here, make sure to create an empty spotify playlist and grab the link
+favs                  = 'https://open.spotify.com/playlist/1gL61UZJBSaoRRiLpHpwhR'
 
 # source playlists
-daily_mix_1           = 'https://open.spotify.com/playlist/37i9dQZF1E36zB6IBvTA1Z'
-daily_mix_2           = 'https://open.spotify.com/playlist/37i9dQZF1E35FNp3AjwoD1'
-daily_mix_3           = 'https://open.spotify.com/playlist/37i9dQZF1E38m7THoFLkuH'
-daily_mix_4           = 'https://open.spotify.com/playlist/37i9dQZF1E3711IypgUARZ'
-daily_mix_5           = 'https://open.spotify.com/playlist/37i9dQZF1E39Ehtni1Mgnb'
-daily_mix_6           = 'https://open.spotify.com/playlist/37i9dQZF1E39ZTGCFZcqaE'
+release_radar         = 'https://open.spotify.com/playlist/37i9dQZEVXbwcRCxi05N0l?si=2171085bec974e9f'
+discover_weekly       = 'https://open.spotify.com/playlist/37i9dQZEVXcJVkLihw7Abc?si=d0a4b8bb042142b8'
+daily_mix_1           = 'https://open.spotify.com/playlist/37i9dQZF1E36zB6IBvTA1Z?si=df8b28825e7c4d21'
+daily_mix_2           = 'https://open.spotify.com/playlist/37i9dQZF1E35FNp3AjwoD1?si=e75cb2cce33b4f75'
+daily_mix_3           = 'https://open.spotify.com/playlist/37i9dQZF1E38m7THoFLkuH?si=81805e3947504b12'
+daily_mix_4           = 'https://open.spotify.com/playlist/37i9dQZF1E3711IypgUARZ?si=b1683163ed1b4290'
+daily_mix_5           = 'https://open.spotify.com/playlist/37i9dQZF1E39Ehtni1Mgnb?si=21ef743b95994866'
+daily_mix_6           = 'https://open.spotify.com/playlist/37i9dQZF1E39ZTGCFZcqaE?si=a2011a94c41a479d'
+
+AcidHouse             = 'https://open.spotify.com/playlist/2NzymlUWKrvI4htixiIs3C'
+BassHouse             = 'https://open.spotify.com/playlist/1NPHecfqYaGA1WePqWokI0'
+Breakbeat             = 'https://open.spotify.com/playlist/6PB0CoDn4povX33QpmQ02e'
+ChillRnBSoul          = 'https://open.spotify.com/playlist/0UtKErAsSYJUHTc2zdnDPX'
+ChillHouse            = 'https://open.spotify.com/playlist/67f5NnFFo2q3L5RYZta5w7'
+ChillhopBeats         = 'https://open.spotify.com/playlist/4Nq24gCbtioyKjAKD1INIL'
+DeepHouse             = 'https://open.spotify.com/playlist/2qyLVxWBh4vQCF9DwCk902'
+Disco                 = 'https://open.spotify.com/playlist/7dxDjILCeSVrjVpzvDv729'
+DiscoHouse            = 'https://open.spotify.com/playlist/2E6LKDR37X50mjgS7pzXsT'
+DrumAndBass           = 'https://open.spotify.com/playlist/4UI4KdZo6nK3rvE2NJffR7'
+FloatHouse            = 'https://open.spotify.com/playlist/54LXniLpAy2UArsQH7i7tX'
+FunkyHouse            = 'https://open.spotify.com/playlist/6oT10NgHvsFLczeWgGgQxd'
+FutureBass            = 'https://open.spotify.com/playlist/00MdAHM17nugGL6n7vdJO8'
+FutureBeats           = 'https://open.spotify.com/playlist/5RucBBOskc1Hk4zTEeO0xR'
+FutureHouse           = 'https://open.spotify.com/playlist/2WHhRbzPCHFr1MNCyLnfPl'
+GroovySoul            = 'https://open.spotify.com/playlist/4vfmQDn8KcjFkitbsXTnx9'
+HalftimeBass          = 'https://open.spotify.com/playlist/7Cx3FApTEjoAJy5gn3m25V'
+HipHopGrime           = 'https://open.spotify.com/playlist/5DNjq57hOo0HJ7oel2nMmI'
+IbizaHouse            = 'https://open.spotify.com/playlist/6D8ZjY9YMfDqBTfAmkWOwj'
+Jazz                  = 'https://open.spotify.com/playlist/0rTmE7jxCO6ojsUue21SS5'
+JazzFunk              = 'https://open.spotify.com/playlist/0KZY0HStJkmOtwkSapnLKj'
+Jungle                = 'https://open.spotify.com/playlist/2edrfJyokLxCWXPnmkHOoa'
+LiquidDrumAndBass     = 'https://open.spotify.com/playlist/1onEsTGHFBsk7IZ5sGSoaE'
+LofiHouse             = 'https://open.spotify.com/playlist/67irKHtgNLMKLgEnWMS93K'
+NuFunk                = 'https://open.spotify.com/playlist/03GNMqql4MbNf8GwsbVFYn'
+ProgressiveHouse      = 'https://open.spotify.com/playlist/6BUxoJGWtllzMdejzgfvHJ'
+TechHouse             = 'https://open.spotify.com/playlist/2FGMn8inH33crqXNTWMdyS'
+Techno                = 'https://open.spotify.com/playlist/12JxGTejUK5wOr7qZDTywX'
+UKGarage              = 'https://open.spotify.com/playlist/6KSZa1AzLgKmrlCruKhmxR'
+
+DrumAndBassFavs       = 'https://open.spotify.com/playlist/1ygT1VOEKgZjzuhrh6CDXB'
+JungleFavs            = 'https://open.spotify.com/playlist/68vNpnlIYpNZQwWG8q6Owc'
+LiquidDrumAndBassFavs = 'https://open.spotify.com/playlist/2gwDNAq63WDUnqhd8lB3XB'
+HalftimeBassFavs      = 'https://open.spotify.com/playlist/2ZQXMeR2KnpPfeZABkMWvQ'
+
+TechHouseFavs         = 'https://open.spotify.com/playlist/4eTR1fDylaA1cmxboyHvFi'
+BassHouseFavs         = 'https://open.spotify.com/playlist/0PbEKyrsS02IgsNwwmJufP'
+DeepHouseFavs         = 'https://open.spotify.com/playlist/4Q1P934932QR8vy9xj4rhr'
+AcidHouseFavs         = 'https://open.spotify.com/playlist/3dNr4pN9sO3FS1ipcItsFT'
+
+IbizaHouseFavs        = 'https://open.spotify.com/playlist/0Lp9Tm5WpZFKRvarL6qNtd'
+DiscoHouseFavs        = 'https://open.spotify.com/playlist/105MPwNzym9UetKNoYljUG'
+FunkyHouseFavs        = 'https://open.spotify.com/playlist/7dRmcCQcYxJYn7anrPZnrW'
+
+FloatHouseFavs        = 'https://open.spotify.com/playlist/3NJAJkhSjwkMV719YwVR19'
+LofiHouseFavs         = 'https://open.spotify.com/playlist/4zd6RUBMoOcvljMKxJDlLs'
+ChillHouseFavs        = 'https://open.spotify.com/playlist/3NIcjGeoe523EgwFref8ra'
+
+FutureHouseFavs       = 'https://open.spotify.com/playlist/0faX1DXvp4VmY97NT0Uv2F'
+ProgressiveHouseFavs  = 'https://open.spotify.com/playlist/3j6IkQNR7EUZPZj8uvuwya'
+FutureBassFavs        = 'https://open.spotify.com/playlist/2RooblzkBz9y7T1uDUi046'
+
+BreakbeatFavs         = 'https://open.spotify.com/playlist/5SO30bU406ejFVOHUcRaaf'
+UKGarageFavs          = 'https://open.spotify.com/playlist/27XBSE3WxBWmll9lGTwJFx'
+HipHopGrimeFavs       = 'https://open.spotify.com/playlist/4LLs77ShlJW9365YtVY868'
+FutureBeatsFavs       = 'https://open.spotify.com/playlist/47PpzzJNVTKb5UBIbFWG1j'
+
+GroovySoulFavs        = 'https://open.spotify.com/playlist/7wuKBNdYd306OTDvKa5RPc'
+ChillRnBSoulFavs      = 'https://open.spotify.com/playlist/5Z3OkNGtSSsHdhN0S9SPKD'
+DiscoFavs             = 'https://open.spotify.com/playlist/3Q14GKIjGkqs7SpSJN5ts3'
+ChillhopBeatsFavs     = 'https://open.spotify.com/playlist/1dnNccPZTcTLfQzxrUmLGB'
+
+JazzFavs              = 'https://open.spotify.com/playlist/7LN7s4SL4ZWOC5fA08n2Cm'
+JazzFunkFavs          = 'https://open.spotify.com/playlist/333oE61YVycXCALSw6iyhF'
+NuFunkFavs            = 'https://open.spotify.com/playlist/54hDW0y41irql0Lx0vTDgT'
 
 config = [
-    { 'from': daily_mix_1, 'to': all_daily_mixes},
-    { 'from': daily_mix_2, 'to': all_daily_mixes},
-    { 'from': daily_mix_3, 'to': all_daily_mixes},
-    { 'from': daily_mix_4, 'to': all_daily_mixes},
-    { 'from': daily_mix_5, 'to': all_daily_mixes},
-    { 'from': daily_mix_6, 'to': all_daily_mixes},
-    # add more merges here
+    { 'from': release_radar,         'to': release_discovery},
+    { 'from': discover_weekly,       'to': release_discovery},
+    { 'from': daily_mix_1,           'to': all_daily_mixes},
+    { 'from': daily_mix_2,           'to': all_daily_mixes},
+    { 'from': daily_mix_3,           'to': all_daily_mixes},
+    { 'from': daily_mix_4,           'to': all_daily_mixes},
+    { 'from': daily_mix_5,           'to': all_daily_mixes},
+    { 'from': daily_mix_6,           'to': all_daily_mixes},
+
+    { 'from': AcidHouse,             'to': deep_mood },
+    { 'from': BassHouse,             'to': clubby_mood },
+    { 'from': Breakbeat,             'to': deep_mood },
+    { 'from': Breakbeat,             'to': breaks_mood },
+    { 'from': ChillRnBSoul,          'to': chill_mood },
+    { 'from': ChillHouse,            'to': chill_mood },
+    { 'from': ChillhopBeats,         'to': chill_mood },
+    { 'from': DeepHouse,             'to': deep_mood },
+    { 'from': Disco,                 'to': upbeat_mood },
+    { 'from': DiscoHouse,            'to': upbeat_mood },
+    { 'from': DiscoHouse,            'to': funky_mood },
+    { 'from': DrumAndBass,           'to': breaks_mood },
+    { 'from': FloatHouse,            'to': deep_mood },
+    { 'from': FunkyHouse,            'to': funky_mood },
+    { 'from': FutureBass,            'to': electronic_mood },
+    { 'from': FutureBeats,           'to': urban_mood },
+    { 'from': FutureHouse,           'to': electronic_mood },
+    { 'from': GroovySoul,            'to': funky_mood },
+    { 'from': GroovySoul,            'to': upbeat_mood },
+    { 'from': HalftimeBass,          'to': electronic_mood },
+    { 'from': HipHopGrime,           'to': urban_mood },
+    { 'from': IbizaHouse,            'to': clubby_mood },
+    { 'from': Jazz,                  'to': chill_mood },
+    { 'from': JazzFunk,              'to': funky_mood },
+    { 'from': Jungle,                'to': breaks_mood },
+    { 'from': LiquidDrumAndBass,     'to': breaks_mood },
+    { 'from': LofiHouse,             'to': deep_mood },
+    { 'from': NuFunk,                'to': funky_mood },
+    { 'from': ProgressiveHouse,      'to': electronic_mood },
+    { 'from': TechHouse,             'to': clubby_mood },
+    { 'from': Techno,                'to': clubby_mood },
+    { 'from': UKGarage,              'to': urban_mood },
+    
+    { 'from': DrumAndBassFavs,       'to': favs},
+    { 'from': JungleFavs,            'to': favs},
+    { 'from': LiquidDrumAndBassFavs, 'to': favs},
+    { 'from': HalftimeBassFavs,      'to': favs},
+    { 'from': TechHouseFavs,         'to': favs},
+    { 'from': BassHouseFavs,         'to': favs},
+    { 'from': DeepHouseFavs,         'to': favs},
+    { 'from': AcidHouseFavs,         'to': favs},
+    { 'from': IbizaHouseFavs,        'to': favs},
+    { 'from': DiscoHouseFavs,        'to': favs},
+    { 'from': FunkyHouseFavs,        'to': favs},
+    { 'from': FloatHouseFavs,        'to': favs},
+    { 'from': LofiHouseFavs,         'to': favs},
+    { 'from': ChillHouseFavs,        'to': favs},
+    { 'from': FutureHouseFavs,       'to': favs},
+    { 'from': ProgressiveHouseFavs,  'to': favs},
+    { 'from': FutureBassFavs,        'to': favs},
+    { 'from': BreakbeatFavs,         'to': favs},
+    { 'from': UKGarageFavs,          'to': favs},
+    { 'from': HipHopGrimeFavs,       'to': favs},
+    { 'from': FutureBeatsFavs,       'to': favs},
+    { 'from': GroovySoulFavs,        'to': favs},
+    { 'from': ChillRnBSoulFavs,      'to': favs},
+    { 'from': DiscoFavs,             'to': favs},
+    { 'from': ChillhopBeatsFavs,     'to': favs},
+    { 'from': JazzFavs,              'to': favs},
+    { 'from': JazzFunkFavs,          'to': favs},
+    { 'from': NuFunkFavs,            'to': favs},
 ]
 
 start(config)
+
